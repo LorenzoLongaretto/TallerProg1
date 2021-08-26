9aece9fcc2a1634011ff03a8f93d50fd18b36f40
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2021-08-26 19:23:28 -0300

    Creo la clase 'BeerHouese' y la clase 'Mesa'

---
+++ TP 1 Programacion Defensiva/Cerveceria/src/main/java/BeerHouse.java
@@ -0,0 +1,17 @@
+
+
+public class BeerHouse {
+    public void abrirLocal(int cantMesas){
+
+    }
+    public Mesa ocuparMesa(int nroMesa){
+        return null;
+    }
+    public float cerrarMesa(int nroMesa){
+        return (float)0.0;
+    }
+
+
+
+
+}
---
+++ TP 1 Programacion Defensiva/Cerveceria/src/main/java/Mesa.java
@@ -0,0 +1,9 @@
+public class Mesa {
+    private char estado;
+    private float total;
+
+    public void cambiaEstado(char nuevoEstado){
+        this.estado=nuevoEstado;
+    }
+    public
+}
