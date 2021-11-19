bfa5e3566d7756b1fcfe5855700389aacd5ec727
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2021-11-19 19:51:03 -0300

    cajuli blanca

--- clinicaTestearTPFinal/src/main/java/factura/Factura.java
+++ clinicaTestearTPFinal/src/main/java/factura/Factura.java
@@ -1,5 +1,8 @@
 package factura;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.GregorianCalendar;
 import java.util.Hashtable;
@@ -35,6 +38,37 @@ public class Factura implements Comparable<Factura> {
     }
 
     public Factura() {
+    }
+    
+    public double calculoImporteAdicionales(int numeroDeFactura, GregorianCalendar fechaDeSolicitud, ArrayList listaDeInsumos) {
+		double respuesta=0;
+		double importeparcial=0,importetotal=0;
+    	if (numeroDeFactura>0) { //xd
+    		 long diasDiferencia=(long)( (fechaDeSolicitud.getTime()-this.fecha.getTime()) /(1000 * 60 * 60 * 24));
+    		if(diasDiferencia<10) {
+    			importeparcial=total-(subtotalimpar*0.8);//falta calcular total y subtotalimpar
+    		}
+    		else {
+    			importeparcial=total*0.7; //B
+    		}
+    		if(this.paciente.getRangoEtario().equals("Mayor")) {
+    			importetotal=importeparcial*1.5; //B
+    		}
+    		else {
+    			importetotal=importeparcial*0.9;
+    		}
+    		if(Math.random()*30+1==Math.random()*30+1) { //ALEATORIO y dia de fecha de facturacion
+    			respuesta=importetotal;
+    		}
+    		else {
+    			if(listaDeInsumos.size()>0) {
+    				respuesta=importetotal+sumaArray();
+    			}
+    		}
+    	}
+    	else
+    		return respuesta;
+    	
     }
 
     public static int getNumFacturaMax() {
